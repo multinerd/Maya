758087623456c77d0b74f527c80ff6caef3e78b1
Multinerd <multinerd@users.noreply.github.com>
2018-01-31 15:26:17 -0500

    Added: CrashReporter.Test

---
+++ Maya.CrashReporter.Test/App.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Threading;
+using Maya.CrashReporter.Model;
+using System.Threading.Tasks;
+
+namespace Maya.CrashReporter.Test
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            if (!Debugger.IsAttached)
+            {
+                AppDomain.CurrentDomain.UnhandledException += CurrentDomainOnUnhandledException;
+                Application.Current.DispatcherUnhandledException += DispatcherOnUnhandledException;
+                TaskScheduler.UnobservedTaskException += TaskSchedulerOnUnobservedTaskException;
+                System.Windows.Forms.Application.ThreadException += Application_ThreadException;
+            }
+        }
+
+        private void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
+        {
+            ReportCrash(e.Exception);
+            Environment.Exit(0);
+        }
+
+        private void TaskSchedulerOnUnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
+        {
+            ReportCrash(e.Exception);
+            Environment.Exit(0);
+        }
+
+        private void DispatcherOnUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            ReportCrash(e.Exception);
+            Environment.Exit(0);
+        }
+
+        private static void CurrentDomainOnUnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            ReportCrash((Exception)e.ExceptionObject);
+            Environment.Exit(0);
+        }
+
+        public static void ReportCrash(Exception exception, string developerMessage = "")
+        {
+            var crashReport = new CrashReport
+            {
+                FromEmail = "bugreports@email.com",
+                ToEmail = "developer@email.com",
+
+                UserName = "bugreports@email.com",
+                Password = "",
+                SmtpHost = "",
+                Port = 587,
+                EnableSSL = true,
+
+                IncludeScreenshot = true,
+                CaptureType = CaptureType.Screen,
+                DeveloperMessage = developerMessage,
+                Exception = exception,
+            };
+            crashReport.Send(exception);
+        }
+    }
+}
---
+++ Maya.CrashReporter.Test/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Windows;
+
+namespace Maya.CrashReporter.Test
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            buttonCrash.Click += ButtonCrash_Click;
+        }
+
+        private void ButtonCrash_Click(object sender, RoutedEventArgs e)
+        {
+            var thread = new Thread(ThrowException);
+            thread.Start();
+        }
+
+        private void ThrowException()
+        {
+            try
+            {
+                throw new ArgumentException();
+            }
+            catch (ArgumentException argumentException)
+            {
+                const string path = "test.txt";
+                try
+                {
+                    if (!File.Exists(path))
+                    {
+                        throw new FileNotFoundException("File Not found when trying to write argument exception to the file", argumentException);
+                    }
+                }
+                catch (Exception exception)
+                {
+                    App.ReportCrash(exception, "");
+                }
+            }
+        }
+    }
+}
