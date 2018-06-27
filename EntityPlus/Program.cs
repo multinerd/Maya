8ea92d3d5dbbecef223703e6cbad358e22a60c9f
Multinerd <multinerd@users.noreply.github.com>
2018-06-27 11:48:42 -0400

    Added Entity Plus

---
+++ EntityPlus/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityPlus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new MikeDevEntities();
+
+
+            //var results = context.Artists.ToList();
+
+            //Console.WriteLine(results);
+        }
+    }
+}
