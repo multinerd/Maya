9fd4c9b4e200bf49ca98017d41d9016acd55213a
Multinerd <multinerd@users.noreply.github.com>
2018-07-05 16:09:05 -0400

    Updated maya.prism docs

--- Maya.Prism/Extensions/ViewModelLocationProviderExtensions.cs
+++ Maya.Prism/Extensions/ViewModelLocationProviderExtensions.cs
@@ -5,13 +5,10 @@ namespace Maya.Prism.Extensions
 {
     public static class ViewModelLocationProviderExtensions
     {
-        /// <summary>
-        /// Bind a ViewModel to a View by name.
-        /// https://stackoverflow.com/a/37045539/5434784
-        /// </summary>
+        /// <summary> Bind a ViewModel to a View by name. https://stackoverflow.com/a/37045539/5434784</summary>
         /// <param name="container"></param>
-        /// <typeparam name="TView"></typeparam>
-        /// <typeparam name="TViewModel"></typeparam>
+        /// <typeparam name="TView">The type of the view.</typeparam>
+        /// <typeparam name="TViewModel">The type of the elements of the view model.</typeparam>
         public static void RegisterViewModelForView<TView, TViewModel>(this IUnityContainer container)
         {
             ViewModelLocationProvider.Register(typeof(TView).ToString(), () => container.Resolve<TViewModel>());
