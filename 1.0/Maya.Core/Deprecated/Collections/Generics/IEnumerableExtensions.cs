57c7c61f58473158f306622861da0bd6c3dde0e4
Multinerd <multinerd@users.noreply.github.com>
2018-07-08 17:35:10 -0400

    Move 'IEnumerableExtensions' to 2.0

---
+++ 1.0/Maya.Core/Deprecated/Collections/Generics/IEnumerableExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Maya.Core.Collections.Generics
+{
+    /// <summary>
+    /// A collection of extensions that target <see cref="IEnumerable"/>
+    /// </summary>
+    public static class IEnumerableExtensions
+    {
+        /// <summary>Creates a <see cref="T:System.Collections.ObjectModel.ObservableCollection`1" /> from an <see cref="T:System.Collections.Generic.IEnumerable`1" />.</summary>
+        /// <param name="source">The <see cref="T:System.Collections.Generic.IEnumerable`1" /> to create an <see cref="T:System.Collections.ObjectModel.ObservableCollection`1" /> from.</param>
+        /// <typeparam name="TSource">The type of the elements of <paramref name="source" />.</typeparam>
+        /// <returns>A <see cref="T:System.Collections.ObjectModel.ObservableCollection`1" /> that contains elements from the input sequence.</returns>
+        [Obsolete("Switch to the 'Maya.Core' 2.0", true)]
+        public static ObservableCollection<TSource> ToObservableCollection<TSource>(this IEnumerable<TSource> source)
+        {
+            return new ObservableCollection<TSource>(source);
+        }
+    }
+}
--- 2.0/Maya.Core/Collections/Generic/IEnumerableExtensions.cs
+++ 2.0/Maya.Core/Collections/Generic/IEnumerableExtensions.cs
@@ -1,22 +1,20 @@
 ﻿using JetBrains.Annotations;
-using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
 
-namespace Maya.Core.Collections.Generics
+namespace Maya.Core.Collections.Generic
 {
     /// <summary>
-    /// A collection of extensions that target <see cref="IEnumerable"/>
+    /// A collection of extensions that target <see cref="T:System.Collections.Generic.IEnumerable`1"/>
     /// </summary>
-    [UsedImplicitly]
+    [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+    // ReSharper disable once InconsistentNaming
     public static class IEnumerableExtensions
     {
         /// <summary>Creates a <see cref="T:System.Collections.ObjectModel.ObservableCollection`1" /> from an <see cref="T:System.Collections.Generic.IEnumerable`1" />.</summary>
         /// <param name="source">The <see cref="T:System.Collections.Generic.IEnumerable`1" /> to create an <see cref="T:System.Collections.ObjectModel.ObservableCollection`1" /> from.</param>
         /// <typeparam name="TSource">The type of the elements of <paramref name="source" />.</typeparam>
         /// <returns>A <see cref="T:System.Collections.ObjectModel.ObservableCollection`1" /> that contains elements from the input sequence.</returns>
-        [UsedImplicitly]
         public static ObservableCollection<TSource> ToObservableCollection<TSource>(this IEnumerable<TSource> source)
         {
             return new ObservableCollection<TSource>(source);
