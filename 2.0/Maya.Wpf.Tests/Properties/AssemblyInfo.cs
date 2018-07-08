623d34d28e809d71de749466de79f843dbdd3536
Multinerd <multinerd@users.noreply.github.com>
2018-07-08 19:16:35 -0400

    Maya.Wpf: Added EditableModelBase and Tests

---
+++ 2.0/Maya.Wpf.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Maya.Wpf.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Maya.Wpf.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e6d51579-b9fc-4493-ac6d-f06ad4a80f92")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ 2.0/Maya.Wpf/EditableModelBase.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel;
+
+// https://web.archive.org/web/20160202215105/http://indepthdev.azurewebsites.net:80/2010/10/reusable-editablemodelbase-using-ieditableobject/
+namespace Maya.Wpf
+{
+    /// <inheritdoc />
+    public abstract class EditableModelBase<TModel> : IEditableObject
+    {
+        private TModel Cache { get; set; }
+
+        private object CurrentModel => this;
+
+        /// <inheritdoc />
+        public void BeginEdit()
+        {
+            Cache = Activator.CreateInstance<TModel>();
+            foreach (var info in CurrentModel.GetType().GetProperties())
+            {
+                if (!info.CanRead || !info.CanWrite) continue;
+                var oldValue = info.GetValue(CurrentModel, null);
+                Cache.GetType().GetProperty(info.Name)?.SetValue(Cache, oldValue, null);
+            }
+        }
+
+        /// <inheritdoc />
+        public void EndEdit()
+        {
+            Cache = default(TModel);
+        }
+
+        /// <inheritdoc />
+        public void CancelEdit()
+        {
+            foreach (var info in CurrentModel.GetType().GetProperties())
+            {
+                if (!info.CanRead || !info.CanWrite) continue;
+                var oldValue = info.GetValue(Cache, null);
+                CurrentModel.GetType().GetProperty(info.Name)?.SetValue(CurrentModel, oldValue, null);
+            }
+        }
+
+    }
+}
